4887dac440cf9419511a2fe170f8b34711654148
ALUMNO <ALUMNO@M07-CII>
2025-03-20 08:16:52 -0600

    Agregar archivos de proyecto.

---
+++ FEMM20250320.WebAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FEMM20250320.WebAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ FEMM20250320.WebAppMVC/Models/Warehouse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FEMM20250320.WebAppMVC.Models;
+
+public partial class Warehouse
+{
+    public int WarehouseId { get; set; }
+
+    public string WarehouseName { get; set; } = null!;
+
+    public string? Notes { get; set; }
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
